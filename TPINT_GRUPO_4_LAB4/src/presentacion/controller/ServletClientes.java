198d987e360816f710fc9685e4a497cbc456da49
shionx9 <55811800+shionx9@users.noreply.github.com>
2021-07-14 16:51:57 -0300

    Revert "Add files via upload"
    This reverts commit bb15ed7e1752e8565a40d1e2a75bc943fa75327f.

--- TPINT_GRUPO_4_LAB4/src/presentacion/controller/ServletClientes.java
+++ TPINT_GRUPO_4_LAB4/src/presentacion/controller/ServletClientes.java
@@ -1,6 +1,7 @@
 package presentacion.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -9,46 +10,65 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
-
 import entidad.Cliente;
 import negocioImpl.ClienteNegImpl;
 import negocio.ClienteNeg;
 
-import entidad.*;
-import datosImpl.*;
-import datos.*;
-
-
 @WebServlet("/ServletClientes")
-public class ServletClientes extends HttpServlet {
+public class ServletClientes extends HttpServlet 
+{
 	private static final long serialVersionUID = 1L;
-    
+	
 	ClienteNeg cliNeg = new ClienteNegImpl();
   
-    public ServletClientes() {
+    public ServletClientes()
+    {
         super();
     }
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-
-		
-		
-		
-		
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
+		if (request.getParameter("inicio")!=null)
+		{
+			RequestDispatcher dispatcher = request.getRequestDispatcher("Home.jsp");
+			dispatcher.forward(request, response);
+		}
+		if (request.getParameter("BtHistorial")!=null)
+		{
+			RequestDispatcher dispatcher = request.getRequestDispatcher("Historial.jsp");
+			dispatcher.forward(request, response);
+		}
+		if (request.getParameter("btTransferencias")!=null)
+		{
+			RequestDispatcher dispatcher = request.getRequestDispatcher("Transferencias.jsp");
+			dispatcher.forward(request, response);
+		}
+		if (request.getParameter("btSolicitarPrestamo")!=null)
+		{
+			RequestDispatcher dispatcher = request.getRequestDispatcher("GestorPrestamos.jsp");
+			dispatcher.forward(request, response);
+		}
+		if (request.getParameter("btPrestamosPedidos")!=null)
+		{
+			RequestDispatcher dispatcher = request.getRequestDispatcher("PrestamoPedido.jsp");
+			dispatcher.forward(request, response);
+		}
+		if (request.getParameter("btPagarCuotas")!=null)
+		{
+			RequestDispatcher dispatcher = request.getRequestDispatcher("PagarCuota.jsp");
+			dispatcher.forward(request, response);
+		}
 		
 		if(request.getParameter("generar") != null)
 		{
-			Clientes cliente = new Clientes();
-			ClientesDao clienteDao = new ClientesImpl();
+			Cliente cliente = new Cliente();
 			
 			cliente.setDni(request.getParameter("TxtDni"));
-			cliente = clienteDao.Buscar_DNI(cliente);
+			cliNeg.Buscar_DNI(cliente);
 			
-			if(cliente.getCuil() != "") 
+			if(cliente.getCuil() != null)
 			{
-				request.setAttribute("Cliente_DNI", cliente);
+				request.setAttribute("Cliente2", cliente);
 			}
 			else 
 			{
@@ -59,54 +79,87 @@ public class ServletClientes extends HttpServlet {
 			RequestDispatcher rd = request.getRequestDispatcher("/AdminCliente.jsp");
 			rd.forward(request, response);
 		}
-		
 	}
 
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	
-		
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
 		if (request.getParameter("BtLogin")!=null)
 		{
-			if (request.getParameter("user").equals("admin") && request.getParameter("pass").equals("admin")) {
-				
-		    	RequestDispatcher dispatcher = request.getRequestDispatcher("HomeAdmin.jsp");
+			if (request.getParameter("user").equals("admin") && request.getParameter("pass").equals("admin")) 
+			{
+		    	RequestDispatcher dispatcher = request.getRequestDispatcher("Home.jsp");
 				dispatcher.forward(request, response);
-				
 			}
-			else {
-				try {
-					
+			else 
+			{
+				try 
+				{
 					Cliente x = new Cliente();
 					Cliente y = new Cliente();
 					x.setUsuario(request.getParameter("user"));
 					x.setContraseña(request.getParameter("pass"));
 					y = cliNeg.obtenerUsuarioContraseña(x.getUsuario(), x.getContraseña());
 					
-					if (y.getUsuario().equals(request.getParameter("user")) && y.getContraseña().equals(request.getParameter("pass"))) {
-						RequestDispatcher dispatcher = request.getRequestDispatcher("Home.jsp");
+					if (y.getUsuario().equals(request.getParameter("user")) && y.getContraseña().equals(request.getParameter("pass"))) 
+					{
+						RequestDispatcher dispatcher = request.getRequestDispatcher("Index.jsp");
 						dispatcher.forward(request, response);
 					}
-					
-					
-				} catch (Exception e) {
+				} 
+				catch (Exception e)
+				{
 					RequestDispatcher dispatcher = request.getRequestDispatcher("Erroracceso.jsp");
 					dispatcher.forward(request, response);
 				}
-
 			}
-	
-			
-			
 		}
-		
 		
 		if(request.getParameter("crear") != null)
 		{
-			Clientes cliente = new Clientes();
-			ClientesDao clienteDao = new ClientesImpl();
-			clienteDao.cargarCliente(cliente, request);
-			clienteDao.agregarCliente(cliente);
+			Cliente cliente = new Cliente();
+			cliNeg.cargarCliente(cliente, request);
+			boolean [] error_count = {true, false, false, false, false};
+			
+			if(cliNeg.buscar_Coincidencia("Dni_Cli" , request.getParameter("TxtDni").toString()))
+			{
+				error_count[0] = false;
+				error_count[1] = true;
+				cliente.setDni("");
+			}
+			
+			if(cliNeg.buscar_Coincidencia("Cuil_Cli" , request.getParameter("TxtCuil").toString()))
+			{
+				error_count[0] = false;
+				error_count[2] = true;
+				cliente.setCuil("");
+			}
+			
+			if(cliNeg.buscar_Coincidencia("Usuario_Cli" , request.getParameter("TxtUsu").toString()))
+			{
+				error_count[0] = false;
+				error_count[3] = true;
+				cliente.setUsuario("");
+			}
+			
+			if(!request.getParameter("TxtPass").equals(request.getParameter("TxtPass2")))
+			{
+				error_count[0] = false;
+				error_count[4] = true;
+				cliente.setContraseña("");
+			}
+			
+			if(error_count[0])
+			{
+				cliNeg.agregarCliente(cliente);
+				
+				request.setAttribute("Validaciones", error_count);
+			}
+			else 
+			{
+				request.setAttribute("Cliente1", cliente);
+				request.setAttribute("Validaciones", error_count);
+			}
 			
 			RequestDispatcher rd = request.getRequestDispatcher("/AdminCliente.jsp");
 			rd.forward(request, response);
@@ -114,19 +167,63 @@ public class ServletClientes extends HttpServlet {
 		
 		if(request.getParameter("Modificar") != null)
 		{
-			Clientes cliente = new Clientes();
-			ClientesDao clienteDao = new ClientesImpl();
-			clienteDao.cargarCliente(cliente, request);
-			clienteDao.modificarCliente(cliente);
+			Cliente cliente = new Cliente();
+			cliNeg.cargarCliente(cliente, request);
 			
-			String Mensaje = "1";
-			request.setAttribute("Mensaje_Modificar", Mensaje);
+			boolean [] error_count = {true, false, false, false, false};
+			
+			if(cliNeg.buscar_Coincidencia("Cuil_Cli" , request.getParameter("TxtCuil").toString()))
+			{
+				error_count[0] = false;
+				error_count[2] = true;
+				cliente.setCuil("");
+			}
+			
+			if(!request.getParameter("TxtPass").equals(request.getParameter("TxtPass2")))
+			{
+				error_count[0] = false;
+				error_count[4] = true;
+				cliente.setContraseña("");
+			}
+			
+			
+			if(error_count[0])
+			{
+				cliNeg.modificarCliente(cliente);
+				
+				String Mensaje = "1";
+				request.setAttribute("Mensaje_Modificar", Mensaje);
+				
+				request.setAttribute("Validaciones", error_count);
+			}
+			else 
+			{
+				request.setAttribute("Cliente2", cliente);
+				request.setAttribute("Validaciones", error_count);
+			}
 			
 			RequestDispatcher rd = request.getRequestDispatcher("/AdminCliente.jsp");
 			rd.forward(request, response);
 		}
-	 
 		
+		if(request.getParameter("enviar") != null) 
+		{
+			ArrayList<Cliente> clientes = new ArrayList<Cliente>();
+			
+			cliNeg.buscar_Clientes(clientes, request);
+			
+			if(!clientes.isEmpty()) 
+			{
+				request.setAttribute("Array_Clientes", clientes);
+				request.setAttribute("Filas", String.valueOf(clientes.size()));
+			}
+			else
+			{
+				request.setAttribute("Filas", "0");
+			}
+			
+			RequestDispatcher rd = request.getRequestDispatcher("/HomeAdmin.jsp");
+			rd.forward(request, response);
+		}
 	}
-
 }
