fb7b0b2254fc806965fa4fd0747e59926b1f5ef8
JoelTraverso <54408217+JoelTraverso@users.noreply.github.com>
2021-07-15 03:53:58 -0300

    agregue los historiales

--- TPINT_GRUPO_4_LAB4/src/datos/MovimientosDao.java
+++ TPINT_GRUPO_4_LAB4/src/datos/MovimientosDao.java
@@ -8,4 +8,7 @@ public interface MovimientosDao {
 	public ArrayList<Movimientos> Obtener_Tabla();
 	public double Egresos(String ini, String fin);
 	public double Ingresos(String ini, String fin);
+	public ArrayList<String> HistorialGeneral();
+	public ArrayList<Integer> CuentasHG(String opcion);
+	public ArrayList<Movimientos> BMovimientos(String ini, String fin,String tipo,double min,double max,int cant,String cue);
 }
