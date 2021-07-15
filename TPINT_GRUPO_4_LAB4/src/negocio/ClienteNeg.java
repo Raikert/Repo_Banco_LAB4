2665d70c5316b12f96b997ddc06c303a7ce1e235
RamaRamon <87089639+RamaRamon@users.noreply.github.com>
2021-07-15 02:24:01 -0300

    Add files via upload

--- TPINT_GRUPO_4_LAB4/src/negocio/ClienteNeg.java
+++ TPINT_GRUPO_4_LAB4/src/negocio/ClienteNeg.java
@@ -1,5 +1,6 @@
 package negocio;
 
+import java.sql.ResultSet;
 import java.util.ArrayList;
 
 import javax.servlet.http.HttpServletRequest;
@@ -9,6 +10,8 @@ import entidad.Cliente;
 public interface ClienteNeg {
 
 	public Cliente obtenerUsuarioContraseña(String usuario,String contraseña);
+	public ResultSet obtenerCuentas(String dni);
+	
 	public Cliente cargarCliente(Cliente cliente, HttpServletRequest request);
 	public void agregarCliente (Cliente cliente);
 	public Cliente Buscar_DNI (Cliente cliente);
