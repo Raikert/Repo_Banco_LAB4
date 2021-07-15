43e539582f219d343ef1cf2f1285c075ffbf62ea
JoelTraverso <54408217+JoelTraverso@users.noreply.github.com>
2021-07-15 03:52:40 -0300

    agregue los servlets que faltaban

--- TPINT_GRUPO_4_LAB4/src/presentacion/controller/ServletMovimientos.java
+++ TPINT_GRUPO_4_LAB4/src/presentacion/controller/ServletMovimientos.java
@@ -14,6 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import datos.*;
 import datosImpl.*;
+import entidad.Movimientos;
 
 /**
  * Servlet implementation class ServletMovimientos
@@ -70,6 +71,121 @@ public class ServletMovimientos extends HttpServlet {
 			rd.forward(request, response);
 		}
 		
+		if(request.getParameter("cargar")!=null)
+		{
+			MovimientosDao mdao = new MovimientosDaoImpl();
+			String cl="";
+			if(request.getParameter("cliente")!="")
+			{
+				cl=request.getParameter("cliente");
+			}
+			request.setAttribute("cl", cl);
+			RequestDispatcher rd = request.getRequestDispatcher("HistorialGeneral.jsp");
+			rd.forward(request, response);
+		}
+		
+		if(request.getParameter("Cargar3")!=null)
+		{
+			MovimientosDao mdao = new MovimientosDaoImpl();
+			ArrayList<Movimientos> lista = new ArrayList<Movimientos>();
+
+			String ini;
+			String fin;
+			double min;
+			double max;
+			String tip;
+			int can;
+			String cue;
+			if(request.getParameter("TxtFecIni")!="")
+			{
+				ini=request.getParameter("TxtFecIni");
+			}else
+			{
+				ini="1900-01-01";
+			}
+			if(request.getParameter("TxtFecFin")!="")
+			{
+				fin=request.getParameter("TxtFecFin");
+			}else
+			{
+				fin="2100-01-01";
+			}
+			if(request.getParameter("TxtMin")!="")
+			{
+				min=Double.parseDouble(request.getParameter("TxtMin"));
+			}else
+			{
+				min=0;
+			}
+			if(request.getParameter("TxtMax")!="")
+			{
+				max=Double.parseDouble(request.getParameter("TxtMax"));
+			}else
+			{
+				max=99999999;
+			}
+				tip=request.getParameter("tipo");
+				can=Integer.parseInt(request.getParameter("RdCant"));
+				cue=request.getParameter("cuenta");
+			
+			lista=mdao.BMovimientos(ini, fin,tip, min, max, can,cue);
+			
+			request.setAttribute("lista", lista);
+			RequestDispatcher rd = request.getRequestDispatcher("HistorialGeneral.jsp");
+			rd.forward(request, response);
+		}
+		
+		if(request.getParameter("Cargar4")!=null)
+		{
+			MovimientosDao mdao = new MovimientosDaoImpl();
+			ArrayList<Movimientos> lista = new ArrayList<Movimientos>();
+
+			String ini;
+			String fin;
+			double min;
+			double max;
+			String tip;
+			int can;
+			String cue;
+
+			if(request.getParameter("TxtFecIni")!="")
+			{
+				ini=request.getParameter("TxtFecIni");
+			}else
+			{
+				ini="1900-01-01";
+			}
+			if(request.getParameter("TxtFecFin")!="")
+			{
+				fin=request.getParameter("TxtFecFin");
+			}else
+			{
+				fin="2100-01-01";
+			}
+			if(request.getParameter("TxtMin")!="")
+			{
+				min=Double.parseDouble(request.getParameter("TxtMin"));
+			}else
+			{
+				min=0;
+			}
+			if(request.getParameter("TxtMax")!="")
+			{
+				max=Double.parseDouble(request.getParameter("TxtMax"));
+			}else
+			{
+				max=99999999;
+			}
+				tip=request.getParameter("tipo");
+				can=Integer.parseInt(request.getParameter("RdCant"));
+				cue=request.getParameter("cuenta");
+
+			lista=mdao.BMovimientos(ini, fin,tip, min, max, can,cue);
+			
+			request.setAttribute("lista", lista);
+			RequestDispatcher rd = request.getRequestDispatcher("Historial.jsp");
+			rd.forward(request, response);
+		}
 	}
 
 }
