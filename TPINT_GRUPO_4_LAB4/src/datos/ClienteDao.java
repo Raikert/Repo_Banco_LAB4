a3d4e3fc6c46239fe29d3aa9533cb04ee248d640
RamaRamon <87089639+RamaRamon@users.noreply.github.com>
2021-07-21 21:38:33 -0300

    Add files via upload

--- TPINT_GRUPO_4_LAB4/src/datos/ClienteDao.java
+++ TPINT_GRUPO_4_LAB4/src/datos/ClienteDao.java
@@ -6,11 +6,19 @@ import java.util.ArrayList;
 import javax.servlet.http.HttpServletRequest;
 
 import entidad.Cliente;
+import entidad.Movimientos;
 
 public interface ClienteDao 
 {
 	public Cliente obtenerUsuarioContraseña(String usuario,String contraseña);
 	public ResultSet obtenerCuentas(String dni);
+	public ArrayList<Movimientos> obtenerMovimientosCuenta(String cuenta);
+	public String obtenerSaldoCuenta(String cuenta);
+	public String comprobarSaldo(String cuenta);
+	public String comprobarCuenta(String cuenta);
+	public boolean grabarMovimiento(Movimientos mov);
+	public boolean actualizarCuentas(Float saldo,String cuenta);
+	public boolean actualizarCuentas2(Float saldo,String cuenta);
 	
 	public void agregarCliente (Cliente cliente);
 	public void cargar_Cliente(Cliente cliente, ResultSet rs);
